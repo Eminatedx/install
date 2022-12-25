24df4d8589281e08af2f6015ee944dc99dcecf7c
Eminatedx <47473539+Eminatedx@users.noreply.github.com>
2022-12-25 08:18:01 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+!#bin/bash
+
+curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install
+sudo ./app service start
+./app status
+sleep 20
+sudo ./apps/gaganode/gaganode config set --token=mkxecbewxhtinxvarvsqzszk
+./app restart

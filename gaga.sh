79efc5eeede83217cfc658b83c2ce481b2f5ea83
David Chang Lee <47473539+Eminatedx@users.noreply.github.com>
2022-12-26 19:46:01 +0800

    Update gaga.sh

--- gaga.sh
+++ gaga.sh
@@ -3,5 +3,5 @@ curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/a
 sudo ./app service start
 ./app status
 sleep 20
-sudo ./apps/gaganode/gaganode config set --token=ukkwfcpknkzmqlwdepujkhkt
+sudo ./apps/gaganode/gaganode config set --token=anwulfvcncvvxvjzrbkfdsml
 ./app restart

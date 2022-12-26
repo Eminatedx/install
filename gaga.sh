28bb1de745c5b8eafa3fe591b3108009d804fc5d
David Chang Lee <47473539+Eminatedx@users.noreply.github.com>
2022-12-27 00:36:50 +0800

    ALVIN TOKEN

--- gaga.sh
+++ gaga.sh
@@ -3,5 +3,5 @@ curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/a
 sudo ./app service start
 ./app status
 sleep 20
-sudo ./apps/gaganode/gaganode config set --token=anwulfvcncvvxvjzrbkfdsml
+sudo ./apps/gaganode/gaganode config set --token=gbfqxiujlgvcicnxyqyqbwax
 ./app restart

899bbc5672fd94a9f7f7a810f84d09ebdbe1fdc3
David Chang Lee <47473539+Eminatedx@users.noreply.github.com>
2023-01-02 01:54:38 +0800

    Update mea.sh

--- mea.sh
+++ mea.sh
@@ -1,4 +1,11 @@
 #!bin/bash
 wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
-sudo ./meson_cdn config set --token=qxouzrqkdkfggibjstqektmg --https_port=443 --cache.size=30
+sudo ./meson_cdn config set --token=rmgorwcirxsskdwohavmxadz --https_port=443 --cache.size=30
 sudo ./service start meson_cdn
+
+curl -o app-linux-amd64.tar.gz https://assets.coreservice.io/public/package/22/app/1.0.3/app-1_0_3.tar.gz && tar -zxf app-linux-amd64.tar.gz && rm -f app-linux-amd64.tar.gz && cd ./app-linux-amd64 && sudo ./app service install
+sudo ./app service start
+./app status
+sleep 20
+sudo ./apps/gaganode/gaganode config set --token=ujgxzkjfyxhxjbfagmztkbqq
+./app restart
